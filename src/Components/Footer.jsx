770fe0d6d4d0650b79557890299b5bc730ed5485
RATHNA01 <rathnasubramaniyan01@gmail.com>
2024-10-01 21:59:43 +0530

    complete

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+           <footer className='py-1 bg-dark'>
+            <div className='container'>
+                <p className='m-0 text-center text-white'>
+                    copyright &copy; Rathna 2024
+                </p>
+            </div>
+           </footer>
+        </div>
+    );
+};
+
+export default Footer;
